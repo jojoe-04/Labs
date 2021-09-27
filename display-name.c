beb571cd3d3b7a3d0a19eadeea18c96e3d559f06
jojoe-04 <jojoe.ainoo@turntabl.com>
2021-09-27 09:02:43 +0000

    C program to take user name & display it

---
+++ display-name.c
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	
+	string name;
+	std:: cout << "Please Enter Your Name: \n";
+	std:: cin >> name;
+	std::cout << "Your name: " << name << "\n";
+}
+
