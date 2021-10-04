f8d4a0b89a5552348605bc629c797ef573a9c14d
root <root@DESKTOP-IL7GSIP.localdomain>
2021-10-04 09:54:05 +0000

    Java Random

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,7 +1,29 @@
-// Your First Program
+
 package Labs.Java;
+
 class HelloWorld {
+
+    /**
+     * Returns the number if the number is even
+     * 
+     * @param num
+     * @return
+     */
+    public static int evaluateEven(int num) {
+        return num;
+    }
+
+    public static double generateRandom() {
+        double rand = Math.random();
+        return rand;
+    }
+
+    public static int generateRandomInt() {
+        return 4;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+        System.out.println("Random Number: " + generateRandom());
     }
 }
