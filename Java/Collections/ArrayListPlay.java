94eabde4ecb72d8b54101f320c06af0942e16974
jojoe-04 <jojoe.ainoo@turntabl.com>
2021-10-07 11:09:32 +0000

    arraylist implementation

---
+++ Java/Collections/ArrayListPlay.java
@@ -0,0 +1,39 @@
+package Labs.Java.Collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayListPlay {
+
+    public static int generateRandom(int max, int min) {
+        int rand = (int) (Math.random() * (max - min) + min); // convert number to random int
+        return rand;
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> listSix = new ArrayList<Integer>();
+        for (int i = 0; i < 5; i++) {
+            int newRand = generateRandom(49, 1);
+            boolean contains = listSix.contains(newRand);
+
+            if (contains) {
+                System.out.println("reapedt");
+            } else {
+                listSix.add(newRand);
+            }
+
+        }
+
+        // sort order of elements
+        // Arrays.sort(arrayOfSix);
+        Collections.sort(listSix);
+        for (int each : listSix) {
+            System.out.println(each);
+        }
+
+        // insertion sort approach
+
+    }
+
+}
