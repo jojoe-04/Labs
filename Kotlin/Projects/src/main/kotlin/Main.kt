3d5ce8690a402b5b3c03cf6a8891af65e3a18ba0
jojoe-04 <jojoe.ainoo@turntabl.com>
2021-10-15 11:12:19 +0000

    SettinG uP iNTELLIJ

---
+++ Kotlin/Projects/src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+var funds : Double = 100.0
+val pswd = "password"
+
+fun main() {
+    var input : String
+    var cmd : List<String>
+
+    while (true) {
+        print("Command: ")
+        input = readLine()!!
+        cmd = input.split(" ")
+        when (cmd[0]) {
+            // Each command goes here...
+
+            else -> println("Invalid command")
+        }
+    }
+}
