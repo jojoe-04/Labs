90abc2f46d3d8337067bc5680a28b077cac28a01
jojoe-04 <jojoe.ainoo@turntabl.com>
2021-10-15 09:48:21 +0000

    Kotlin Lab2

--- Kotlin/Learn/HellloWolrd.kt
+++ Kotlin/Learn/HellloWolrd.kt
@@ -25,10 +25,78 @@ fun variableExplore(){
     // var name: String = "John" // String specify
     // val birthyear: Int = 1975 // Int specify
 
-    // println(name)
-    // println(birthyear)
+    val myNum: Int = 5                // Int
+    val myDoubleNum: Double = 5.99    // Double
+    val myLetter: Char = 'D'          // Char
+    val myBoolean: Boolean = true     // Boolean
+    val myText: String = "Hello"      // String
 
     println(name)          // Print the value of name
     println(birthyear) 
     
+}
+
+fun typeConversion(){
+    // toByte(), toShort(), toInt(), toLong(), toFloat(), toDouble() or toChar()
+    val x: Int = 5;
+    val y: Long = x.toLong();
+    println(y)
+}
+
+fun stringManipulation(){
+    var txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    println("The length of the txt string is: " + txt.length)
+
+    var txt = "Hello World"
+    println(txt.toUpperCase())   // Outputs "HELLO WORLD"
+    println(txt.toLowerCase())   // Outputs "hello world
+
+    var txt1 = "Hello World"
+    var txt2 = "Hello World"
+    println(txt1.compareTo(txt2))  // Outputs 0 (they are equal)
+
+    var txt = "Please locate where 'locate' occurs!"    
+    println(txt.indexOf("locate"))  // Outputs 7
+
+    var firstName = "John"
+    var lastName = "Doe"
+    println(firstName + " " + lastName)
+
+
+}
+
+fun conditions(){
+    val time = 20
+    val greeting = if (time < 18) "Good day." else "Good evening."
+    println(greeting)
+}
+
+fun whenStatement(){
+    val day = 4
+    val result = when (day) {
+    1 -> "Monday"
+    2 -> "Tuesday"
+    3 -> "Wednesday"
+    4 -> "Thursday"
+    5 -> "Friday"
+    6 -> "Saturday"
+    7 -> "Sunday"
+    else -> "Invalid day."
+    }
+    println(result)
+}
+
+
+
+fun learnArrays(){
+    val cars = arrayOf("Volvo", "BMW", "Ford", "Mazda")
+    println(cars[0])
+    println(cars.size)
+    if ("Volvo" in cars) println("It exists!")
+
+    //ranges
+    for (nums in 5..15) {
+        println(nums)
+    }
+
 }
