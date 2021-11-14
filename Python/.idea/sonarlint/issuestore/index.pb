8ea2a3908fd0fc24762b8ed2783287a47b500856
Jojoe Ainoo <jojoeainoo@Obsidians-MacBook-Pro.local>
2021-11-14 21:21:27 +0000

    Lectures

---
+++ Python/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,8 @@
+
+:
+
+Labs.ipynb,c/c/ccb1604edfc3f9bdcc6cebce9dc620c35e41f20f
+J
+Data Science/HelloWorld.py,2/1/215fc3d18e1a2459a4f71fc43cd085d3a5552951
+M
+Data Science/HelloWorld.ipynb,4/a/4a0e655999a32b1d048c16dc509f46e126be1b8a
--- Scala/src/main/scala/SemlrLecture.scala
+++ Scala/src/main/scala/SemlrLecture.scala
@@ -146,5 +146,169 @@ object OOP extends App {
   case class Cash(pounds:Int, pence:Int);
   val in = Cash(20,0);
   println(in);
+}
+
+
+object inheritance extends App{
+  class Animal(var size:Int, var colour:String){
+   def this() = this (0,"New");
+   def move(distance:Int): Int  = distance + 1;
+   def eat() = "All";
+   def reproduce(): Animal = new Animal();
+ }
+
+  class Lion(var maneColour:String, s:Int, c:String ) extends Animal(s,c) {
+    def roar:String = "Grrr!";
+    def hunt:Int = this.size + 1;
+    override def eat() = "Meat";
+  }
+
+  val lion : Animal = new Lion("Brown",87,"red");
+  println(lion move 2);
+}
+
+object traits extends App{
+  abstract class Animal;
+
+  trait Swimmer {
+    def swim = "splash";
+  }
+
+  trait Flyer {
+    def fly = "whee";
+  }
+
+  class Bird extends Animal with Flyer;
+  class Fish extends Animal with Swimmer;
+  class Penguin extends Animal with Flyer with Swimmer;
+
+  val hen = new Bird;
+  val salmon = new Fish;
+  val penguin = new Penguin;
+
+  println(s"hen: ${hen.fly}");
+  println(s"salmon: + ${salmon.swim}");
+  println(s"Penguin: ${penguin.fly} , ${penguin.swim}");
+
+  trait Ordered[A] extends java.lang.Comparable[A] {
+    def < (that: A): Boolean = compareTo(that) < 0;
+    def > (that: A): Boolean = compareTo(that) > 0;
+  }
+}
+
+object PatternMatching extends App{
+  def whatType(a: Any): String = {
+    a match {
+      case i: Int => "Int type"
+      case d: Double => "Double type"
+      case s: String => "String type"
+      case u:java.net.URL => "Url type"
+      case _ => "Something"
+    }
+  }
+
+  println(PatternMatching whatType 4);
+  println(PatternMatching whatType "4");
+  println(PatternMatching whatType 4.0);
+  println(PatternMatching whatType new java.net.URL("http://www.oamarkets.com"));
+  println(whatType(2 to 5));
+}
+
+object FileMgt extends App {
+  import java.io._
+  var input: FileReader = _; // file reader object
+  try{
+    input = new FileReader("file.txt"); // read file
+    println("opened");
+  }
+  catch{
+    case fileNotFoundException: FileNotFoundException => println("No Such File Directory");
+    case ioe: IOException => println(ioe.getMessage);
+    case _ => println("Something Else");
+  }
+  finally{
+    input.close();
+  }
+}
+
+
+object Functional extends App{
+
+  val doubleIt  = (x:Int) => x * 2;
+  println(Functional doubleIt 2);
+
+  val squareIt = (x:Int) => x * x;
+  println(Functional squareIt 2);
+
+  val doIt = (x:Int, f:(Int => Int)) => f(x); // high order functions
+  println(doIt(2,doubleIt));
+  println(doIt(2,squareIt));
+
+
+  // passing in anonymous function
+  println(doIt(2, (x=>x+1)));
+  println(doIt(2, _ + 1));
+
+  // returning functions
+  val multBy = (n:Int) => ((x:Int) => x *n);
+  val double = multBy(2);
+  val triple = multBy(3)
+  println(double(4));
+  println(triple(4));
+
+  // names function
+  val username = (n:String) => ((l:String) => n + l);
+  val first = username("Emmanuel");
+  println(first("joj"));
+
+
+  // function composition
+  val f = (x:Int) => x + 2;
+  val g = (x:Int) => x * 2;
+
+  val h = f compose g;
+  println(h(2));
+
+  val u = g compose f;
+  println(u(2));
+
+  val k = f andThen g;
+  println(k(2))
+}
+
+
+object collections extends App{
+  val range1 = 1 to 5;
+  range1.foreach(x => print(x +" "));
+  println(range1.forall(_ < 10));
+  println(range1.exists(_ == 2));
+
+
+  val nameList = List("Joj","Emma","Rita")
+  val upper = nameList.map(_.toUpperCase())
+
+  val range3 = 1 to 10
+  val evenOdd = range3 partition (_ % 2 == 0)
+
+
+
+  val squares = {for (i <- 1 to 10) yield i * i }
+  val squares2 = (1 to 10).map(i => i * i)
+
+  val nestedSq = Seq(Seq(1,2,3,4,5),Seq(4,6,7,8,9))
+  println(nestedSq.flatten)
+
+
+  val odd1 = (1 to 10).filter(i => i % 2 != 0)
+  val even1 = for(i <- 1 to 10 if( i % 2 == 0)) yield i
+  val (even, odd) = (1 to 10) partition (_ % 2 == 0)
+
+
+  val nestFor = for (i <- 1 to 5; j <- 5 to 6) yield i + j
+
+
+
+
+
 
 }
